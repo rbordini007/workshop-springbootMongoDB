8ce77e4ee3e47eb1d7d1155a1b67a952add12cf7
rbordini007 <ricardo.sampaio@edu.unipar.br>
2020-02-05 19:06:58 -0300

    created the exceptions

--- src/main/java/com/devric/workshopmongo/resources/UserResource.java
+++ src/main/java/com/devric/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.devric.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devric.workshopmongo.domain.User;
+import com.devric.workshopmongo.dto.UserDTO;
 import com.devric.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +22,19 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){		
+	public ResponseEntity<List<UserDTO>> findAll(){		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO>listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	
+	
+	
 }
--- src/main/java/com/devric/workshopmongo/services/UserService.java
+++ src/main/java/com/devric/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.devric.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devric.workshopmongo.domain.User;
 import com.devric.workshopmongo.repositories.UserRepository;
+import com.devric.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("not found Object"));
 	}
 }
